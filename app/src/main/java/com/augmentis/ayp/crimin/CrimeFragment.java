25426dcf750ee7731afbe5b03c1737c71201f419
Nutdanai Prom <Nutdanai.p@augmentis.biz>
2016-07-25 17:16:08 +0700

    Crime_recycleView

--- app/src/main/java/com/augmentis/ayp/crimin/CrimeFragment.java
+++ app/src/main/java/com/augmentis/ayp/crimin/CrimeFragment.java
@@ -3,7 +3,10 @@ package com.augmentis.ayp.crimin;
 import android.annotation.TargetApi;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.UUID;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -24,17 +27,35 @@ import android.widget.EditText;
  * Created by Nutdanai on 7/18/2016.
  */
 public class CrimeFragment extends Fragment{
+    private static final String CRIME_ID = "CrimeFragment.CRIME_ID";
+    private static final String CRIME_POSITION = "CrimeFragment.CRIME_POS";
     private Crime crime;
+    private int position;
+    private EditText editText;
     private Button crimeDateButton;
     private CheckBox crimeSolvedCheckbox ;
-    private EditText editText;
+    public CrimeFragment(){}
+
+    public static CrimeFragment newInstance(UUID crimeId, int position){
+        Bundle  args = new Bundle();
+        args.putSerializable(CRIME_ID,crimeId);
+        args.putInt(CRIME_POSITION,position);
+
+        CrimeFragment crimeFragment = new CrimeFragment();
+        crimeFragment.setArguments(args);
+        return  crimeFragment;
+    }
 
 
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        crime =  new Crime();
+        UUID crimeId =(UUID) getArguments().getSerializable(CRIME_ID);
+        position  = getArguments().getInt(CRIME_POSITION);
+        crime = CrimeLab.getInstance().getCrimeById(crimeId) ;
+//        Log.d(CrimeListFragment.TAG,"crime.getId()=" + crime.getId());
+        Log.d(CrimeListFragment.TAG,"crime.getTitle()=" + crime.getTitle());
     }
 
 
@@ -42,6 +63,7 @@ public class CrimeFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_crime,container,false);
         editText = (EditText) v. findViewById(R.id.crime_title);
+        editText.setText(crime.getTitle());
         editText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int cout, int after) {
@@ -63,17 +85,22 @@ public class CrimeFragment extends Fragment{
         crimeDateButton.setEnabled(false);
 
         crimeSolvedCheckbox = (CheckBox) v.findViewById(R.id.crime_solved);
+        crimeSolvedCheckbox.setChecked(crime.isSolved());
         crimeSolvedCheckbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
                 crime.setSolved(isChecked);
-                Log.d(CrimeActivity.TAG,"Crime" + crime.toString());
+                Log.d(CrimeListFragment.TAG,"Crime" + crime.toString());
             }
         });
 
-
+        Intent intent = new Intent();
+        intent.putExtra("position",position);
+        getActivity().setResult(Activity.RESULT_OK,intent);
         return v;
     }
+
+
 
     private String  getFormattedDate(Date date){
         return new SimpleDateFormat("dd MMMM yyyy").format(date);
